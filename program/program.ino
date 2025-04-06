33fb00cde603759767baea947dab50b44eef03bb
jx32 <jorgeefernandezz@gmail.com>
2025-04-05 20:50:19 -0600

    Update calls post history message Add seconds of day sync

--- program/program.ino
+++ program/program.ino
@@ -16,8 +16,15 @@ WiFiClient client;
 
 // Flags and responses from API
 bool isIrrigating = false;
+
 bool waitingForGetDeviceResponse = false;
 String getDeviceResponse = "";
+
+bool waitingForPatchDeviceResponse = false;
+String patchDeviceDescription = "";
+String patchDeviceKey = "";
+
+bool waitingForActivationSync = false;
 
 // Irrigation configuration
 long activationSeconds = 120;
@@ -29,19 +36,21 @@ long activationSecondsCounter = 0;
 long irrigationSecondsCounter = 0;
 long configurationCheckCounter = 0;
 
+/*
+Original ones
 #define WIFI_PIN 0
 #define OPEN_VALVE_PIN 1
 #define VALVE_PIN 3
 #define TICK_PIN 2
+*/
 
-void resetActionCounters() {
-  activationSecondsCounter = 0;
-  irrigationSecondsCounter = 0;
-  Serial.println("Action counters resetted");
-}
+#define WIFI_PIN 3
+#define OPEN_VALVE_PIN 2
+#define VALVE_PIN 5
+#define TICK_PIN 4
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   //while (!Serial);
 
   pinMode(WIFI_PIN, OUTPUT);
@@ -51,12 +60,17 @@ void setup() {
 
   digitalWrite(TICK_PIN, LOW);
 
+  digitalWrite(OPEN_VALVE_PIN, HIGH);
+  digitalWrite(TICK_PIN, HIGH);
+
   reconnectToWifi();
 
   Serial.println("You're connected to the network");
 
-  closeValve("Irrigation stopped", false);
   getDevice();
+
+  digitalWrite(OPEN_VALVE_PIN, LOW);
+  digitalWrite(TICK_PIN, LOW);
 }
 
 void reconnectToWifi() {
@@ -81,7 +95,7 @@ void reconnectToWifi() {
 }
 
 void checkForIrrigationEnablement() {
-  if (activationSecondsCounter >= activationSeconds) {
+  if (activationSecondsCounter == activationSeconds) {
     openValve();
   }
 }
@@ -109,6 +123,15 @@ void loop() {
     }
 
     readGetDevice();
+
+  } else if (waitingForPatchDeviceResponse) {
+
+    readPatchLastUpdate();
+
+  } else if (waitingForActivationSync) {
+
+    readSyncActivationCounter();
+
   } else {
     digitalWrite(TICK_PIN, HIGH);
     delay(500);
@@ -131,21 +154,98 @@ void loop() {
       Serial.println(irrigationSecondsCounter);
       checkForIrrigationDisablement();
     }
+
+    resetActivationSecondsCounterOnMidnight();
+  }
+}
+
+void resetActivationSecondsCounterOnMidnight() {
+  if (activationSecondsCounter >= 86400) { // 86400 = 24:00:00
+    activationSecondsCounter = 0;
+    Serial.println("Activation seconds counter resetted to 0 due to midnight");
+  }
+}
+
+void readPatchLastUpdate () {
+  while (client.available()) {
+      String responseLine = client.readStringUntil('\n');
+      
+      if (responseLine.indexOf("HTTP/1.1") > -1) {
+        client.stop();
+        waitingForPatchDeviceResponse = false;
+
+        if (!isRequestOk(responseLine)) {
+          Serial.println("Invalid Patch device HTTP code: " + responseLine);
+          patchLastUpdate(patchDeviceDescription, patchDeviceKey);
+        } else if (!isIrrigating) {
+          // Resync activation counter
+          syncActivationCounter();
+        }
+      }
+  }
+}
+
+void readSyncActivationCounter () {
+  while (client.available()) {
+      String responseLine = client.readStringUntil('\n');
+      //Serial.println(responseLine);
+      
+      if (responseLine.indexOf("HTTP/1.1") > -1) {
+        waitingForActivationSync = false;
+
+        if (!isRequestOk(responseLine)) {
+          Serial.println("Invalid sync activation counter HTTP code: " + responseLine);
+          delay(5000);
+          syncActivationCounter();
+        }
+      }
+
+      if (responseLine.indexOf("{") > -1) {
+        JsonDocument doc;
+        
+        // Deserialize the JSON document
+        DeserializationError error = deserializeJson(doc, responseLine);
+
+        if (error) {
+          Serial.println("Invalid sync activation JSON deserialization error - " + responseLine);
+          delay(5000);
+          waitingForActivationSync = false;
+          syncActivationCounter();
+        } else {
+          activationSecondsCounter = doc["seconds"].as<long>();
+
+          Serial.println("Activation synced to " + String(activationSecondsCounter) + " sec.");
+          waitingForActivationSync = false;
+          client.stop();
+        }
+      }
+  }
+}
+
+void syncActivationCounter () {
+  reconnectToWifi();
+
+  waitingForActivationSync = false;
+
+  if (client.connected()) {
+    client.stop();
   }
 
-  // if the server's disconnected, stop the client:
-  /*if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
+  if (client.connectSSL(server, 443)) {
+    Serial.println("Connected to server");
+    // Make the HTTP request
+    client.println("GET /api/v1/sync-time HTTP/1.1");
+    client.println("Host: lazy-garden-api.onrender.com");
+    client.println("Connection: close");
+    client.println();
+    Serial.println("Sync activation counter Request sent");
+
+    waitingForActivationSync = true; // Tell arduino to catch the data
+  } else {
+    Serial.println("Cannot connect to the server, retrying");
     client.stop();
-
-    // Turn off all API response waiting flags
-    waitingForGetDeviceResponse = false;
-
-    // do nothing forevermore:
-    //for(;;)
-    //  ;
-  }*/
+    syncActivationCounter();
+  }
 }
 
 void getDevice() {
@@ -184,7 +284,7 @@ void readGetDevice() {
     // Just process line by lines
     if ((int) c == 10 || (int) c == 13) {
 
-      Serial.println(getDeviceResponse);
+      //Serial.println(getDeviceResponse);
 
       if (waitingForGetDeviceResponse && getDeviceResponse.indexOf("HTTP/1.1") > -1 && !isRequestOk(getDeviceResponse)) {
         // Wrong requests causes to stop fetching the configuration
@@ -206,8 +306,8 @@ void readGetDevice() {
         //Serial.println(error.f_str());
       } else if (getDeviceResponse.indexOf("{") > -1) { // Ensure that the line contains a JSON object
         if (waitingForGetDeviceResponse) {
-          Serial.print("Get Device Response: ");
-          Serial.println(getDeviceResponse);
+          //Serial.print("Get Device Response: ");
+          //Serial.println(getDeviceResponse);
 
           waitingForGetDeviceResponse = false;
 
@@ -223,7 +323,7 @@ void readGetDevice() {
 }
 
 bool isRequestOk(String responseCodeLine) {
-  return responseCodeLine.indexOf("200") > -1;
+  return responseCodeLine.indexOf("200 OK") > -1;
 }
 
 void updateConfiguration(JsonDocument serverDeviceDoc) {
@@ -234,23 +334,22 @@ void updateConfiguration(JsonDocument serverDeviceDoc) {
     if (serverActivationSeconds != activationSeconds) {
       updateMessage += "Activation seconds updated from " + String(activationSeconds) + "s to " + String(serverActivationSeconds) + "s. ";
       activationSeconds = serverActivationSeconds;
-      resetActionCounters();
     }
 
     if (serverIrrigateSeconds != irrigateSeconds) {
       updateMessage += "Irrigate seconds updated from " + String(irrigateSeconds) + "s to " + String(serverIrrigateSeconds) + "s. ";
       irrigateSeconds = serverIrrigateSeconds;
-      resetActionCounters();
     }
 
     if (updateMessage != "") {
-      closeValve(updateMessage, false);
+      closeValve(updateMessage, true);
+    } else {
+      syncActivationCounter();
     }
 }
 
-void -closeValve(String message, bool notifyAPI) {
-  // TODO: Send signal to close the water valve
-  resetActionCounters();
+void closeValve(String message, bool notifyAPI) {
+  irrigationSecondsCounter = 0;
   isIrrigating = false;
   digitalWrite(OPEN_VALVE_PIN, LOW);
   digitalWrite(VALVE_PIN, LOW);
@@ -264,30 +363,30 @@ void -closeValve(String message, bool notifyAPI) {
 void openValve() {
   digitalWrite(OPEN_VALVE_PIN, HIGH);
   digitalWrite(VALVE_PIN, HIGH);
-  // Update device using the API
-  patchLastUpdate("Irrigating your plants", "IRRIGATING");
-
-  // TODO: Send signal to open the water valve
-  resetActionCounters();
   isIrrigating = true;
+
+  // Update device using the API
+  patchLastUpdate("Irrigating your plants", "IRRIGATION_STATUS");
 }
 
-void patchLastUpdate(String message, String state) {
+void patchLastUpdate(String description, String key) {
   reconnectToWifi();
+
+  waitingForPatchDeviceResponse = false;
 
   if (client.connected()) {
     client.stop();
   }
 
-  Serial.println("Updating device with message: " + message + " and state: " + state);
+  Serial.println("Posting history with description: " + description + " and key: " + key);
   // TODO: Call API to patch error message and add last update date
 
   if (client.connectSSL(server, 443)) {
-    String body = "{\"error\": \"" + message + "\",\"state\": \"" + state + "\"}";
+    String body = "{\"deviceId\": \"" + deviceId + "\", \"key\": \"" + key + "\", \"description\": \"" + description + "\"}";
 
     Serial.println("Connected to server");
     // Make the HTTP request
-    client.println("PATCH /api/v1/device/" + deviceId + " HTTP/1.1");
+    client.println("POST /api/v1/device/" + deviceId + "/history HTTP/1.1");
     client.println("Host: lazy-garden-api.onrender.com");
     client.println("Connection: close");
     client.println("Content-Type: application/json");
@@ -295,13 +394,21 @@ void patchLastUpdate(String message, String state) {
     client.println(body.length());
     client.println();
     client.println(body);
-    Serial.println("Patch Device Request sent with body " + body);
+    Serial.println("Post history Request sent with body " + body);
+
+    waitingForPatchDeviceResponse = true;
+
+    // Put backup data in case of any retry
+    patchDeviceDescription = description;
+    patchDeviceKey = key;
   } else {
-    Serial.println("Cannot connect to the server");
+    Serial.println("Cannot connect to the server, retrying");
+    client.stop();
+    patchLastUpdate(description, key);
   }
 }
 
-void printData() {
+/*void printData() {
   Serial.println("Board Information:");
   // print your board's IP address:
   IPAddress ip = WiFi.localIP();
@@ -318,4 +425,4 @@ void printData() {
   Serial.print("signal strength (RSSI):");
   Serial.println(rssi);
 
-}
+}*/
